5d5c33bce38f877763759d29044ddf6bb7192cbc
assanethiao <thiaoassanethiao@gmail.com>
2026-02-02 16:50:50 +0100

    mes réalisationbs en TP JS

---
+++ 2.Exercices JS Serie 2025/script5.js
@@ -0,0 +1,6 @@
+let tabImages = ['chat.jpg', 'chien.jpg', 'tigre.jpg', 'lion.jpg'];
+
+const imgElement = document.querySelectorAll('img');
+const buttons = document.querySelectorAll('button');
+const upButton = buttons[0];
+const downButton = buttons[1];
